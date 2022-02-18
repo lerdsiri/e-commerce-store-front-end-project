ba18f366fdca1c15641b53c6efdf4a578cd695b2
Siri <siri.lerdsirisopon@gmail.com>
2022-02-18 13:12:30 +0100

    removed unused code

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,5 +1,4 @@
 import CartTable from "components/CartTable/CartTable";
-import CheckoutArea from "components/CheckoutArea/CheckoutArea";
 import BackLink from "components/BackLink/BackLink";
 import 'pages/Cart/Cart.css'
 
@@ -9,7 +8,6 @@ export default function Cart() {
     return (
         <div className='cart-page'>
             <CartTable />
-            <CheckoutArea />
             <BackLink />
         </div>
     );
